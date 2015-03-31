a60f5d1145161c7cf29fc84e95ad3a8d7d1526a1
Mohammed Ali <mrali243@hotmail.com>
2015-04-01 01:36:14 +0200

    Knoppen naar andere vensters laten gaan

--- Code/Trivia/src/trivia/HoofdmenuController.java
+++ Code/Trivia/src/trivia/HoofdmenuController.java
@@ -6,10 +6,15 @@
 package trivia;
 
 import java.awt.Button;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -17,7 +22,6 @@ import javafx.fxml.Initializable;
  * @author Rehman
  */
 public class HoofdmenuController implements Initializable {
-    
 
     /**
      * Initializes the controller class.
@@ -25,11 +29,23 @@ public class HoofdmenuController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
+    /**
+     * Deze methode sluit het programma af.
+     */
     @FXML
     private void exit() {
         System.exit(1);
     }
-    
+
+    @FXML
+    private void spelOpzettenKnop() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/SpelOpzetten.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
 }
--- Code/Trivia/src/trivia/Trivia.java
+++ Code/Trivia/src/trivia/Trivia.java
@@ -6,9 +6,11 @@
 package trivia;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
@@ -16,13 +18,12 @@ import javafx.stage.Stage;
  * @author Nick
  */
 public class Trivia extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/views/Hoofdmenu.fxml"));
-        
+        Parent root = FXMLLoader.load(getClass().getResource("/views/Splashscreen.fxml"));
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.show();
     }
@@ -33,5 +34,5 @@ public class Trivia extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
